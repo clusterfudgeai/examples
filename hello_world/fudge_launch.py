b31b14276225b9c591215d5957bfa0d1adeec9b4
George Thomas <georgetamayothomas@gmail.com>
2024-04-04 17:11:11 +0100

    comment hello_world example

--- hello_world/fudge_launch.py
+++ hello_world/fudge_launch.py
@@ -9,14 +9,34 @@ def main():
         clusterfudge.CreateLaunchRequest(
             name="hello-world-example",
             description="Sleep 60 on 2 replicas with 1 CPU each.",
+            # jobs is a list of jobs that are scheduled together
+            # i.e. gang-scheduled.
             jobs=[
                 clusterfudge.Job(
+                    # short_name must be unique within the same launch.
+                    # It is used to identify the job in the dashboard.
                     short_name="hello_world",
+                    # replicas=2 requests two copies of this job. The requested
+                    # processes may run on the same or different machines.
                     replicas=2,
+                    # processes is a list of processes that run concurrently on
+                    # a single machine. There can be any number of processes
+                    # per job.
                     processes=[
                         clusterfudge.Process(
                             command=["python3", "-c", workload],
+                            # resource_requirements specifies the resources
+                            # required for this process.
+                            # This can be empty, in which case the workload
+                            # can be scheduled on any machine.
                             resource_requirements=clusterfudge.Resources(
+                                # cpus=1 requests 1 CPU.
+                                # other resources we currently support are:
+                                # - a100_40gb
+                                # - a100_80gb
+                                # - h100
+                                # - rtx3090
+                                # - t4
                                 cpus=1,
                             ),
                         ),
