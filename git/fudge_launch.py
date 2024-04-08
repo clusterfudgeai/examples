107945b945360cc17e29521572a63ae9d50243a8
George Thomas <georgetamayothomas@gmail.com>
2024-04-08 22:43:24 +0900

    git: add git example

---
+++ git/fudge_launch.py
@@ -0,0 +1,36 @@
+import clusterfudge
+
+
+def main():
+    client = clusterfudge.Client()
+    launch = client.create_launch(
+        clusterfudge.CreateLaunchRequest(
+            name="git example",
+            description="Example showing how we can clone git repos",
+            deployment=clusterfudge.GitRepo(
+                repo="git@github.com:clusterfudgeai/examples.git",
+                branch="main",
+            ),
+            jobs=[
+                clusterfudge.Job(
+                    short_name="git",
+                    replicas=1,
+                    processes=[
+                        clusterfudge.Process(
+                            command=[
+                                "python3",
+                                "git/hello_git.py",
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        )
+    )
+
+    print(f"Launched: launch/{launch.launch_id} 🎉")
+    print(f"https://clusterfudge.clusterfudge.com/dashboard/launches/")
+
+
+if __name__ == "__main__":
+    main()
